aa73a3c0b2f7552d89182ccdb8b9fb2bdf3aeec7
Ivan <ivansurmanov@yandex.ru>
2022-12-21 23:49:20 +0300

    task4

--- 1.py
+++ 1.py
@@ -2,4 +2,3 @@ import requests
 
 response = requests.get(url='http://httpbin.org/')
 print(type(response))
-17
--- 4.3.py
+++ 4.3.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
 
-with open('C:/Users/Ivan/PycharmProjects/pars/index.html', 'r', encoding='utf-8') as file:
+with open('C:/Users/Ivan/PycharmProjects/python_pars/index.html', 'r', encoding='utf-8') as file:
     soup = BeautifulSoup(file, 'lxml')
     print(soup)
